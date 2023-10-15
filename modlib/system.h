c2aef230fc4004ead3969840e9cc384daccb6720
Aren Elchinyan <a2.dev@yandex.ru>
2023-10-15 18:44:29 +0300

    Изменен пример

---
+++ modlib/system.h
@@ -0,0 +1,50 @@
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+typedef unsigned long long uint64_t;
+typedef long long int64_t;
+
+typedef struct {
+	void (*fb_printf)(char *str, ...);
+} env_t;
+
+typedef struct {
+	int reserved;
+} framebuffer_t;
+
+typedef struct {
+	int reserved;
+} sys_info_t;
+
+typedef struct {
+	char *name;
+	void *addr;
+} module_func_t;
+
+typedef struct {
+	char *name;
+	char *message;
+	int err_code;
+	uint64_t func_count;
+	module_func_t *func[];
+} module_info_t;
+
+typedef struct {
+	int reserved;
+} func_t;
+
+typedef struct {
+	uint8_t a[4];
+	uint8_t b[4];
+	uint8_t c[4];
+	uint8_t d[4];
+} uid_t;
+
+typedef struct {
+	uint16_t year;
+	uint8_t month;
+	uint8_t day;
+	uint8_t second;
+} time_t;
+
+void (*fb_printf)(char *str, ...);
--- modules/helloworld/build.sh
+++ modules/helloworld/build.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 echo "Название: Hello world"
 echo "Лицензия: Публичное достояние"
-g++ -O0 -finput-charset=UTF-8 -fexec-charset=cp1251 -c -fPIC -nostdlib main.cpp -o hello.o
+gcc -I../../modlib -O0 -finput-charset=UTF-8 -fexec-charset=cp1251 -c -fPIC -nostdlib main.c -o hello.o
 gcc -shared -nostdlib hello.o -o hello.so -Wl,--entry=_start
 echo "Сборка завершена, файл: hello.so"
