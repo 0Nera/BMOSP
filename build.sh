7edf669601bfd701a0eed672cd00701cfa0cfc78
Aren Elchinyan <a2.dev@yandex.ru>
2023-11-02 00:26:32 +0300

    Начато составление инструкции по разработке пользовательских модулей

--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@ cd helloworld/ && chmod +x build.sh && ./build.sh && cd ..
 cd music/ && chmod +x build.sh && ./build.sh && cd ..
 cd simd/ && chmod +x build.sh && ./build.sh && cd ..
 cd cpubench/ && chmod +x build.sh && ./build.sh && cd ..
+cd pci/ && chmod +x build.sh && ./build.sh && cd ..
 cd ..
 python3 pbuild.py
