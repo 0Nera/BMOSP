25b77bb8bbae207605a90c008e20ddc50efa00c0
Aren <aren@synapseos.ru>
2023-11-17 21:09:26 +0300

    Создание библиотеки модулей

---
+++ modlib/modstd.h
@@ -0,0 +1,22 @@
+/**
+ * modstd.h
+ * Стандартная библиотека для модулей
+ *
+ * Заголовочный файл содержащий определения стандартной библиотеки модулей
+ *
+ */
+
+#ifndef MODSTD_H
+#define MODSTD_H
+
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+typedef unsigned long long uint64_t;
+
+typedef char int8_t;
+typedef short int16_t;
+typedef int int32_t;
+typedef long long int64_t;
+
+#endif // modstd.h
