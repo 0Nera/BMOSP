469acb67823af6e4c97d4216867dffac6d234fc7
Aren <a.elchinyan@itelma.su>
2023-11-22 19:16:03 +0300

    Теперь модули автоматически собираются без указания пути

--- build.sh
+++ build.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
+
 cd modules/
 mkdir -p bin
 dos2unix */*.sh
-#cd helloworld/ && chmod +x build.sh && ./build.sh && cd ..
-#cd music/ && chmod +x build.sh && ./build.sh && cd ..
-cd simd/ && chmod +x build.sh && ./build.sh && cd ..
-cd cpubench/ && chmod +x build.sh && ./build.sh && cd ..
-cd pci/ && chmod +x build.sh && ./build.sh && cd ..
-cd pci_data/ && chmod +x build.sh && ./build.sh && cd ..
+chmod +x */build.sh
+
+for dir in */; do
+    if [ $dir != "bin/" ]; then
+        cd $dir && ./build.sh && cd ..
+    fi
+done
+
 cd ..
+
 python3 pbuild.py
