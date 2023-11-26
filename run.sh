96b3696cf68b29d57c2faf5d41e94edc43ade729
Aren Elchinyan <a2.dev@yandex.ru>
2023-11-26 18:24:13 +0300

    Запуск через SATA

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-qemu-system-x86_64 -name "БМПОС" -cpu max -m 1G -smp 1 -hda bmosp.hdd  --no-reboot
+qemu-system-x86_64 -name "БМПОС" -cpu max -m 1G -smp 1 \
+ -drive file=bmosp.hdd,if=none,id=sata_drive -device ahci \
+ -device virtio-blk-pci,drive=sata_drive --no-reboot 
+#qemu-system-x86_64 -name "БМПОС" -cpu max -m 1G -smp 1 -hda bmosp.hdd  --no-reboot
 #qemu-system-x86_64 -name "БМПОС" -cpu max -m 1G -smp 1 -cdrom bmosp.iso -boot d  --no-reboot
 #qemu-system-x86_64 -name "БМПОС" -cpu max -m 1G -smp 1 -bios ovmf/OVMF.fd -hda bmosp.hdd -d int --no-reboot
 #qemu-system-x86_64 -name "БМПОС" -cpu host -m 1G -smp 1 -bios ovmf/OVMF.fd -hda bmosp.hdd 
