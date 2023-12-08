3909eb183c88f6972d1cbf675c9a5f2d96813008
Aren <a.elchinyan@itelma.su>
2023-12-08 18:11:59 +0300

    Добавлена отправка word в IO порт

--- include/arch.h
+++ include/arch.h
@@ -31,4 +31,14 @@ static inline uint8_t inb(uint16_t port) {
 	return ret;
 }
 
+static inline void outw(uint16_t port, uint16_t val) {
+	asm volatile("outw %0, %1" : : "a"(val), "Nd"(port));
+}
+
+static inline uint16_t inw(uint16_t port) {
+	uint16_t ret;
+	asm volatile("inw %1, %0" : "=a"(ret) : "Nd"(port));
+	return ret;
+}
+
 #endif // arch.h
